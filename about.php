2f29e29b855658134cfe47ddb2483c7141f42199
Rich Samartino <rich@detroitcitycentral.com>
2015-12-31 15:03:31 -0500

    adding all files

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="styles.css">
+<title>About Voices for Earth Justice</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header>
+<img src="images/voices_logo_color 300.jpg" alt="VEJ logo">
+<h1>About</h1>
+</header>
+
+<nav>
+<?php include_once("nav.html"); ?>
+</nav>
+
+<section>
+<p><strong>Our Vision</strong><br /><br />
+Believing God is revealed in all of Creation, we raise our voices as members of Earth community.  Our planet, our home, is charged with life, and its beauty expresses and nourishes our common spirituality.<br /><br />
+Yet Earth is being threatened by human activity.<br /><br />
+Thus Earth-Keeping is a moral issue based on justice, and we work for right relationships with everything God creates.</p>
+
+<p><strong>Our Rationale</strong><br /><br />
+
+The global environment with its finite resources is a common concern of all peoples. The protection of Earth's vitality, diversity, and beauty is a sacred trust. ---<em>The Earth Charter</em><br /><br />
+
+In Michigan, our soul is not to be found in steel and concrete, or sprawling new housing developments, or strip malls. Rather it is found in the soft petals of a trillium, the gentle whisper of a headwater stream, the vista of a Great Lakes shoreline, and the wonder in children's eyes upon seeing their first bald eagle. ---<em>Former Governor William Milliken</em><br /><br />
+
+Only the sense of the violated honor of the Earth, and the need to restore this honor, will awaken in the human the energies needed to renew the planet in any effective manner. ---<em>Fr. Thomas Berry, CP</em><br /><br />
+
+We can't save the natural world, except by rediscovering the sacred in nature.  ---<em>Dr. Seyyed Hossein Nasr</em><br /><br />
+
+The hidden environment of the human spirit is seldom mentioned in our concern for our more obvious environmental problems. 
+We would do well to carefully reconsider this omission.  ---<em>Vera C. Shaw</em><br /><br />
+
+The Earth is the Lord's and all that it holds, the world and all its inhabitants. ---<em>Psalm 24:1</em><br /><br />
+
+Sustainability is about stabilizing the currently disruptive relationship between Earth's two most complex systems-human culture and the living world. ---<em>Paul Hawken</em></p>
+
+</section>
+
+<footer>
+Copyright 2015 Voices for Earth Justice, nonprofit 501(c)(3). All Rights Reserved.
+</footer>
+
+</body>
+</html>
