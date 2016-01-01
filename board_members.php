f389f4b5abbb806dee177fd76e2358092c4b2928
Rich Samartino <rich@detroitcitycentral.com>
2015-12-31 19:09:06 -0500

    added board members page and finished faith statements page

---
+++ board_members.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="styles.css">
+<title>Board Members - Voices for Earth Justice</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header>
+<img src="images/voices_logo_color 300.jpg" alt="VEJ logo">
+<h1>Board Members</h1>
+</header>
+
+<nav>
+<?php include_once("nav.html"); ?>
+</nav>
+
+<section>
+
+<h2>Board of Directors and Staff</h2>
+
+<div class="floating-box">
+	<strong>Sr. Janet Stankowski, OP – President</strong><br />
+	MEd, MDiv, DMin<br />
+	Student Affairs Coordinator at Madonna University, Livonia
+	Roseville, MI<br />
+	<a href="mailto:opjanet@gmail.com">opjanet@gmail.com</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Doris Allen – Vice President </strong><br />
+	DDS<br />
+	Retired director of Community Dental Center in Ann Arbor, Native American Spirituality Activist.<br />
+	Ypsilanti, MI<br />
+	<a href="mailto:dorisja19@gmail.com">dorisja19@gmail.com</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Catharine Khoury - Secretary</strong><br />
+	RN, MA<br />
+	Parish Nurse at St. James, Ferndale,<br />
+	Environmental Health Educator<br />
+	Madison Heights, MI<br />
+	<a href="mailto:mrsckrn@msn.com">mrsckrn@msn.com</a>
+</div>
+
+<div class="floating-box">
+	<strong>Sr. Virginia King, OP - Treasurer</strong><br />
+	Program Assistant, Institute for Communal Contemplation and Dialogue,<br />
+	Environmental Activist<br />
+	Detroit, MI<br />
+	<a href="mailto:gking43@att.net">gking43@att.net</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Mary Lou Malone</strong><br />
+	BA, MEd<br />
+	Retired educator, leader in the MI Unitarian Universalist Social Justice Network and the Detroit Food Security Network.<br />
+	Detroit, MI<br />
+	<a href="mailto:maryloumaloneuu@gmail.com">maryloumaloneuu@gmail.com</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Jeff Jenks</strong><br />
+	Mayor pro tem, Huntington Woods,<br />
+	Member Temple Beth El, Bloomfield Hills,<br />
+	President of Southeast MI Returned Peace Corps volunteers<br />
+	Huntington Woods, MI<br />
+	<a href="mailto:travelisfun@wowway.com">travelisfun@wowway.com</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Patricia Gillis, Executive Director</strong><br />
+	MA, MSW<br />
+	Administration, urban gardening, and retreat leader<br />
+	Southfield, MI<br />
+	<a href="mailto:voices4earth@juno.com">voices4earth@juno.com</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Rodney Edmonds</strong><br />
+	Training Specialist, Chance for Life,<br />
+	St. Vincent de Paul Justice Initiative, Member Moorish Science Temple of America, Inc.<br />
+	Detroit, MI<br />
+	<a href="mailto:rodneyedmonds93@yahoo.com">rodneyedmonds93@yahoo.com</a> 
+</div>
+
+<div class="floating-box">
+	<strong>Anthony Hatinger</strong><br />
+	BA Religious Studies and Agriculture<br />
+	Garden Production Coordinator and Aquaponics Manager at Central Detroit Christian Community Development Corporation.<br />
+	Detroit, MI<br />
+	<a href="mailto:ahatinger@detcdc.org">ahatinger@detcdc.org</a> 
+</div>
+
+<img class="center" src="images/VEJ_board_photo.jpg" alt="Voices for Earth Justice board members">
+
+</section>
+
+<footer>
+Copyright 2015 Voices for Earth Justice, nonprofit 501(c)(3). All Rights Reserved.
+</footer>
+
+</body>
+</html>
--- faith_statements.php
+++ faith_statements.php
@@ -17,43 +17,48 @@
 </nav>
 
 <section>
-<img src="images/judaism_ecology.jpg" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
+
+<img src="images/judaism_ecology.jpg" alt="Judaism ecology" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
 <p><strong>Judaism and Ecology</strong><br /><br />
 In Genesis (1:28), God commands humans to "fill the world and capture it." The Torah permits us to use the world as we see fit. However, a few verses later (Genesis 2:15), the Torah tempers this by telling us that God put humans in the Garden (symbolic of the entire world) "to work it and to guard it."</p>
 
-<img src="images/christianity_ecology.jpg" style="float:right; border-style: groove; margin: 0 0 0 .5em;">
+<img src="images/christianity_ecology.jpg" alt="Christianity ecology" style="float:right; border-style: groove; margin: 0 0 0 .5em;">
 <p><strong>Christianity and Ecology</strong><br /><br />
 "Then the angel showed me the river of the water of life, bright as crystal, flowing from the throne of God and of the Lamb through the middle of the street of the city; also, on either side of the river, the tree of life with its twelve kinds of fruit, yielding its fruit each month; and the leaves of the tree were for the healing of the nations." ~ Revelation 22:1,2<br /><br />
 
 <strong>National Council of Churches in Christ (NCCC):</strong><br />
 <a href="http://www.ncccusa.org/news/14.02.05theologicalstatement.html" target="_blank">http://www.ncccusa.org/news/14.02.05theologicalstatement.html</a></p>
 
-<img src="images/islam_ecology.jpg" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
+<img src="images/islam_ecology.jpg" alt="Islam ecology" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
 <p><strong>Islam and Ecology</strong><br /><br />
 "It is He who drives the winds, glad tidings heralding His mercy, and We send down pure water from the sky, that We may give life thereby to a dead land and provide drink to what We have created- cattle and humans in great numbers." ~ Qur'an 25:48-9
 "We made from water every living thing, will they not then believe?" ~ Qur'an 21:30</p>
 
-<img src="images/hinduism_ecology.jpg" style="float:right; border-style: groove; margin: 0 0 0 .5em;">
+<img src="images/hinduism_ecology.jpg" alt="Hinduism ecology" style="float:right; border-style: groove; margin: 0 0 0 .5em;">
 <p><strong>Hinduism and Ecology</strong><br /><br />
 <ul>
 	<li><a href="http://www.hinduismtoday.com/pdf_downloads/hindu-climate-change-declaration.pdf" target="_blank">Hindu Declaration on Climate Change</a></li>
 	<li><a href="http://www.hinduismtoday.com/modules/smartsection/item.php?itemid=5161" target="_blank">Hinduism and the Environment</a></li>
 </ul>	
 	</p>
+	
 <div>
-<img src="images/buddhism_ecology.jpg" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
+<img src="images/buddhism_ecology.jpg" alt="Buddhism ecology" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
 <p><strong>Buddhism and Ecology</strong><br /><br />
 His Holiness the Dalai Lama’s speeches and messages on the environment: <a href="http://www.dalailama.com/messages/environment" target="_blank">http://www.dalailama.com/messages/environment</a></p>
 </div>
+
 <div>
-<img src="images/sikhism_ecology.jpg" style="float:right; border-style: groove; margin: 0 0 0 .5em;">
+<img src="images/sikhism_ecology.jpg" alt="Sikhism ecology" style="float:right; border-style: groove; margin: .5em 0 .5em .5em;">
 <p><strong>Sikhism and Ecology</strong><br /><br />
 <a href="http://sikhinstitute.org/khalsa21st/surjeetkaur.html" target="_blank">Sikhism and Environmental Ethics</a></p>
 </div>
+
 <div>
-<img src="images/uu_ecology.jpg" style="float:left; border-style: groove; margin: 0 .5em 0 0;">
+<img src="images/uu_ecology.jpg" alt="Unitarian Universalist ecology" style="float:left; border-style: groove; margin: .5em .5em 0 0;">
 <p><a href="http://www.uua.org/environment/" target="_blank"><strong>Unitarian Universalist Association</strong></a></p>
 </div>
+
 </section>
 
 <footer>
