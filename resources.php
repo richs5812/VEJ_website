cfa3328d84b08e4924de18a3b0236d6723ae2d79
Rich Samartino <rich@detroitcitycentral.com>
2016-01-03 19:56:33 -0500

    added resources page modified navigation so list items display inline block

---
+++ resources.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="styles.css">
+<title>Voices for Earth Justice - Resources</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header>
+<img src="images/voices_logo_color 300.jpg" alt="VEJ logo">
+<h1>Resources</h1>
+</header>
+
+<nav>
+<?php include_once("nav.html"); ?>
+</nav>
+
+<section>
+<h2>Prayer</h2>
+<h3>Prayer includes retreats, reflections, meditation, prayers, poems, etc.</h3>
+<ul>
+	<li>Awakening to God’s call to Earthkeeping (Lutheran/Christian), <a href="http://download.elca.org/ELCA%20Resource%20Repository/Awakening_To_Gods_Call_To_Earthkeeping.pdf" target="_blank">PDF available here</a></li>
+	<li>Evangelical Environmental Network - <a href="http://www.creationcare.org/" target="_blank">http://www.creationcare.org/</a> (also listed under education/study)</li>
+	<li><a href="http://imcw.org/Resources/ArticleDetail/articleType/ArticleView/articleId/58/How-to-Meditate-A-Guide-to-Formal-Sitting-Practice.aspx" target="_blank">How to Meditate</a></li>
+	<li><a href="http://imcw.org/Resources/ArticleDetail/articleType/ArticleView/articleId/58/How-to-Meditate-A-Guide-to-Formal-Sitting-Practice.aspx" target="_blank">How to Meditate</a></li>
+	<li><a href="https://educationforjustice.org/resources/prayer-climate-change" target="_blank">Education for Justice: Prayer for Climate Change</a></li>
+	<li><a href="https://educationforjustice.org/node/1356" target="_blank">Education for Justice: Native American Prayers for the Earth</a></li>
+	<li><a href="http://www.uua.org/environment/engagement/192858.shtml" target="_blank">Unitarian Universalist worship resources</a></li>
+</ul>
+	
+<h2>Education / Study</h2>
+<h3>Education/study includes educational materials, workshops, videos, conferences, books, etc.</h3>
+
+<ul>
+	<li><a href="http://co-intelligence.org/newsletter/" target="_blank">Evolutionary Life</a></li>
+	<li><a href="http://www.earthlight.org/" target="_blank">Earthlight Library</a></li>
+	<li><a href="http://fore.research.yale.edu/religion/" target="_blank">The Forum on Religion and Ecology at Yale</a></li>
+	<li><a href="http://www.arcworld.org/" target="_blank">Alliance of Religions and Conservation</a></li>
+	<ul>
+		<li><a href="http://www.arcworld.org/faiths.asp?pageID=6" target="_blank">Website for Muslims</a></li>
+	</ul>
+	<li><a href="http://www.creationcare.org/" target="_blank">Evangelical Environmental Network</a></li>
+	<li><a href="http://greenfaith.org/resource-center/spirit/religious-environmental-education" target="_blank">GreenFaith</a></li>
+	<li><a href="http://www.journeyoftheuniverse.org/" target="_blank">Journey of the Universe film project</a></li>
+	<li><a href="http://theamericanmuslim.org/tam.php/features/articles/environment_ecology/" target="_blank">American Muslim</a></li>
+	<li><a href="http://blessedtomorrow.org/" target="_blank">Blessed Tomorrow: Interfaith Coalition on Climate Change</a> (also listed under action)</li>
+	<li><a href="http://www.discipleshomemissions.org/dhm/dhm-ministries/environment/" target="_blank">Green Chalice, the Disciples of Christ</a></li>
+</ul>
+
+<h2>Action</h2>
+<h3>Action is ways to get involved in public policy, sustainable living practices like lowering carbon footprints and eliminating toxics from homes, recycling, etc.</h3>
+
+<ul>
+	<li><a href="http://cec-mi.org/" target="_blank">Clean Energy Coalition</a></li>
+	<li><a href="http://www.miipl.org/about-michigan-ipl/" target="_blank">Michigan Interfaith Power and Light</a></li>
+	<li><a href="http://catholicclimatecovenant.org/" target="_blank">Catholic Climate Covenant</a></li>
+	<li><a href="http://www.coejl.org/" target="_blank">Coalition on the Environment and Jewish Life (COEJL)</a></li>
+	<li><a href="http://www.mortgageloan.com/environment/" target="_blank">Green Mortgages—Energy Efficient Mortgage Guide</a></li>
+
+</ul>
+
+</section>
+
+<footer>
+Copyright 2015 Voices for Earth Justice, nonprofit 501(c)(3). All Rights Reserved.
+</footer>
+
+</body>
+</html>
