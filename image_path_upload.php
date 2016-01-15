4a931f94d86a47ba492eaf0d0e3cd37644806a0b
Rich Samartino <rich@detroitcitycentral.com>
2016-01-14 19:34:35 -0500

    upload image paths to database instead of image file

--- image_path_upload.php
+++ image_path_upload.php
@@ -18,7 +18,7 @@
 
 <section>
 
-<form action="path_upload.php" method="post" enctype="multipart/form-data">
+<form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
---
+++ test_path_gallery.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="styles.css">
+<title>Voices for Earth Justice - Gallery</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header>
+<img src="images/voices_logo_color 300.jpg" alt="VEJ logo">
+<h1>Events</h1>
+</header>
+
+<nav>
+<?php include_once("nav.html"); ?>
+</nav>
+
+<section>
+<input type="file" />
+<?php
+include 'db_connect.php';
+
+$query = "select * from VEJ_pics";
+$stmt = $con->prepare( $query );
+
+//$gallery = '12th_earth_day';
+
+//bind the id of the image you want to select
+//$stmt->bindParam(1, $gallery);
+$stmt->execute();
+
+//to verify if a record is found
+$num = $stmt->rowCount();
+//echo $num;
+
+if( $num ){
+	//if found
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+	
+	echo '
+	<div class="img">
+		<a target="_blank" href="uploads/'.$row[fileName].'">
+		<img src="uploads/'.$row[fileName].'" width="300" />
+		</a>
+		<div class="desc">'.$row[caption].'</div>
+	</div>
+	';
+	
+	}
+}else{
+	//if no image found with the given id,
+	//load/query your default image here
+}
+
+//echo "<img src='source.php?id=1' width='400' />";
+
+?>
+</section>
+
+<footer>
+Copyright 2015 Voices for Earth Justice, nonprofit 501(c)(3). All Rights Reserved.
+</footer>
+
+</body>
+</html>
