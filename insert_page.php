686f7c4a00891adfcd27ea60601cd3f2e3faa524
Rich Samartino <rich@detroitcitycentral.com>
2016-01-27 18:21:51 -0500

    created page to insert page info in database

---
+++ insert_page.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="styles.css">
+<title>Voices for Earth Justice - Insert Result</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header>
+<img src="images/voices_logo_color 300.jpg" alt="VEJ logo">
+<h1>Insert result</h1>
+</header>
+
+<nav>
+<?php include_once("nav.html"); ?>
+</nav>
+
+<section>
+<?php
+include 'db_connect.php';
+    
+$query = "INSERT INTO Pages (Title) VALUES (?)";
+$stmt = $con->prepare( $query );
+
+$stmt->bindParam(1, $_POST["pageTitle"]);
+
+$con->beginTransaction();
+if ($stmt->execute() == TRUE) {
+  echo 'Page title inserted successfully.';
+} else {
+	echo "Error: <br>". $stmt->error;
+}$con->commit();
+
+?> 
+
+</section>
+
+<footer>
+Copyright 2015 Voices for Earth Justice, nonprofit 501(c)(3). All Rights Reserved.
+</footer>
+
+</body>
+</html>
---
+++ page_create_edit.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="styles.css">
+<title>Voices for Earth Justice - Create/Edit page</title>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header>
+<img src="images/voices_logo_color 300.jpg" alt="VEJ logo">
+<h1>Create/Edit page</h1>
+</header>
+
+<nav>
+<?php include_once("nav.html"); ?>
+</nav>
+
+<section>
+
+<form action="insert_page.php" method="post">
+
+<label for="pageTitle">Page Title: </label><input type="text" name="pageTitle" id="pageTitle" />
+
+<input type="submit" value="Create Page"/>
+</form>
+
+</section>
+
+<footer>
+Copyright 2015 Voices for Earth Justice, nonprofit 501(c)(3). All Rights Reserved.
+</footer>
+
+</body>
+</html>
