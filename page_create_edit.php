dd80c0819d155c49d8720ada09830e171417c4de
Rich Samartino <rich@detroitcitycentral.com>
2016-02-01 23:22:41 -0500

    insert page can display existing page info

--- page_create_edit.php
+++ page_create_edit.php
@@ -18,15 +18,77 @@
 
 <section>
 
+<!--start drop down menu-->
+<form id="dropDownMenu" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+<select class="dropDown" name="page_id" onchange="change()">
+<option value="">New Page</option>
+
+<?php 
+include 'db_connect.php';
+
+$query = "SELECT page_id, Title FROM Pages ORDER BY Title ASC;";
+$stmt = $con->prepare( $query );
+
+$con->beginTransaction();
+$stmt->execute();
+$con->commit();
+
+$num = $stmt->rowCount();
+
+if( $num ){
+	//if found
+	while ($dropDownRow = $stmt->fetch(PDO::FETCH_ASSOC)){
+		if ($dropDownRow['page_id']==$_POST['page_id']){
+    		$selected = "selected";
+    	}else{
+    		$selected = "";
+    		}
+        echo '<option value="'. $dropDownRow['page_id'] .'" '.$selected.' >'. $dropDownRow['Title'] .'</option>';
+
+	}
+	
+}else{
+	echo 'no results';
+}
+
+?>
+</select>
+
+<script>
+function change(){
+    document.getElementById("dropDownMenu").submit();
+}
+</script>
+
+</form>
+<!--end drop down menu-->
+
 <form action="insert_page.php" method="post">
 
 <br />
-<label for="pageTitle">Page Title: </label><input type="text" name="pageTitle" id="pageTitle" /><br /><br />
+<?php
 
-<label for="OtherNotes">Page Content: </label><textarea id="pageContent" name="pageContent" ></textarea>
+include 'db_connect.php';
+
+$pageQuery = "select Title, Content from Pages WHERE page_id = ?";
+$pageStmt = $con->prepare( $pageQuery );
+
+$pageStmt->bindParam(1, $_POST["page_id"]);
+
+$con->beginTransaction();
+$pageStmt->execute();
+$con->commit();
+
+$pageRow = $pageStmt->fetch(PDO::FETCH_ASSOC);
+
+
+echo '<label for="pageTitle">Page Title: </label><input type="text" name="pageTitle" id="pageTitle" value="'.$pageRow["Title"].'" /><br /><br />
+
+<label for="OtherNotes">Page Content: </label><textarea id="pageContent" name="pageContent" >'.$pageRow["Content"].'</textarea>
 
 <br /><br /><input type="submit" value="Create Page"/>
-</form>
+</form>';
+?>
 
 </section>
 
